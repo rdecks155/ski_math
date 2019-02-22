0628b631310ca671b23dd41ed088ea058ef89880
Andre Hamm <andre.m.hamm@gmail.com>
2019-02-22 14:01:06 -0700

    added stats and game page

--- ski_math/urls.py
+++ ski_math/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
 
 from . import views
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('game/', views.Game.as_view(), name='game'),
+    path('stats/', views.Stats.as_view(), name='stats'),
 ]
--- ski_math/views.py
+++ ski_math/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-
+from django.views.generic.base import TemplateView
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class Game(TemplateView):
+    template_name = 'game.html'
+
+class Stats(TemplateView):
+    template_name = 'stats.html'
