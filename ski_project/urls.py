f738b2eebfc260f163804d600609e4bc15d83aa2
Max Harper <mwharp24@gmail.com>
2019-03-30 13:16:40 -0600

    css stuff

--- ski_project/urls.py
+++ ski_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
